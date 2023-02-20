24bf8372ee7d85733046045d83a903b885a18c9a
aptopspeed <maplestory.ssso@gmail.com>
2023-02-20 08:55:03 +0700

    first commit project

---
+++ src/components/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+type LoginPageProps = {
+  //
+};
+
+const LoginPage: React.FC<any> = () => {
+  return (
+    <>
+      <div>LoginPage</div>
+    </>
+  )
+};
+
+export default LoginPage;
---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  important: "#root",
+  theme: {
+    extend: {},
+    colors: {
+      primary: 'rgb(25,118,210)',
+      secondary: '#ecc94b',
+    }
+  },
+  plugins: [],
+}
